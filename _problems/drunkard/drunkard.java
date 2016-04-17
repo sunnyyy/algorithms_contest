2f48363d9887f31ece754e516c895f3ff9d3525c
Sunnia Ye <sye2@wellesley.edu>
2016-04-17 12:07:32 -0400

    added pracice problems

---
+++ _problems/drunkard/drunkard.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class drunkard {
+
+    public static void dr1() {
+	next = seed;
+	long next = (mult * next) % mod;
+    }
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNextLine()) {
+	    String[] a = sc.nextLine().split(" ");
+	    if (a.length == 3) {
+		
+	    } else {
+		
+	    }
+	}
+    }
+
+}
---
+++ _problems/fractions/fractions.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+public class fractions {
+
+    public static String fra(String s) {
+        boolean b = false;
+	double f = Double.parseDouble(s);
+	if (s.length() < 17) {
+	    int l = 17-s.length();
+	    for (int i = 0; i < l; i++) {
+		s += "0";
+	    }
+	}
+	outer:
+	for (int d = 1; d <= 1000; d++) {
+	    for (int n = 1; n <= d-1; n++) {
+		if ( Math.abs(f - (double)n/d) < 0.0000000005 ) {
+		    b = true;
+		    s += " " + n + "/" + d;
+		    break outer;
+		}
+	    }
+	}
+	if (b == false) {
+	    s += " -";
+	}
+	return s;
+    }
+
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	while (sc.hasNextLine()) {
+	    System.out.println(fra(sc.nextLine()));
+	}
+    }
+
+}
